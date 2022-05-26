18f915f7a2abcaf73858cf2d14f38e172cb5b7ca
Chase Meadors <c.ed.mead@gmail.com>
2022-05-26 01:15:03 -0600

    Update deps; update to Vue 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './styles/theme.scss';
-import Vue from 'vue';
+
 import App from './components/App.vue';
+import { createApp } from 'vue';
 
 const body = document.getElementsByTagName('body')[0];
 const app = document.createElement('div');
 app.setAttribute('id', 'app');
 body.insertBefore(app, body.firstChild);
-new Vue({
-    el: '#app',
-    render: h => h(App),
-});
+
+createApp(App).mount('#app');
